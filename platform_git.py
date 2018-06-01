7db1f09781115010087803af131d72c9504da199
John Mercouris <john@mercouris.email>
2018-06-01 17:11:56 +0200

    example of how to use with git repository

--- platform_git.py
+++ platform_git.py
@@ -2,6 +2,9 @@
 # This file provides an example of the broadcast API using Git           #
 ##########################################################################
 
+import configparser
+from git import Repo
+
 
 def broadcast_data(encrypted_message):
     """Write the encrypted_message to the git repository
@@ -11,8 +14,20 @@ def broadcast_data(encrypted_message):
     :rtype: None
     
     """
-    with open("encrypted_ip", 'wb') as encrypted_ip:
+    settings = configparser.ConfigParser()
+    settings.read('server_configuration.ini')
+    
+    repository_path = settings.get('platform-configuration', 'repository-path')
+    
+    with open("{}/encrypted".format(repository_path), 'wb') as encrypted_ip:
         encrypted_ip.write(encrypted_message)
+    
+    repo = Repo(repository_path)
+    repo.index.add(["{}/encrypted".format(repository_path)])
+    repo.index.commit("update")
+    
+    origin = repo.remote(name='origin')
+    origin.push()
 
 
 def read_data():
@@ -22,6 +37,15 @@ def read_data():
     :rtype: bytes
     
     """
-    with open("encrypted_ip", 'rb') as encrypted_ip:
+    settings = configparser.ConfigParser()
+    settings.read('server_configuration.ini')
+    
+    repository_path = settings.get('platform-configuration', 'repository-path')
+    repo = Repo(repository_path)
+    origin = repo.remote(name='origin')
+    origin.pull()
+    
+    with open("{}/encrypted".format(repository_path), 'rb') as encrypted_ip:
         data = encrypted_ip.read()
+    
     return data
