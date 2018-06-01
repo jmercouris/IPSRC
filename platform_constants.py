1db0c955348c7613cb854be25a5d32f7822ef286
John Mercouris <john@mercouris.email>
2018-06-01 16:48:05 +0200

    add git to platform constants

--- platform_constants.py
+++ platform_constants.py
@@ -1,11 +1,16 @@
 import platform_disk
+import platform_git
 
 
 def get_broadcast_function(platform_string):
     if(platform_string == "disk"):
         return platform_disk.broadcast_data
+    if(platform_string == "git"):
+        return platform_git.broadcast_data
 
 
 def get_read_function(platform_string):
     if(platform_string == "disk"):
         return platform_disk.read_data
+    if(platform_string == "git"):
+        return platform_git.read_data
